ef8c80cbe008965f8ef3fa4d56dd42c8405034c3
azqddn <aziquddin99@gmail.com>
2024-12-12 01:24:12 +0800

    aziq try

---
+++ app/Http/Controllers/CatalogueController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogueController extends Controller
+{
+    // public function testing()
+    // {
+    //     return "Testing route works!";
+    // }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\CatalogueController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/**
+ * 
+ * Manage Catalogue
+ * 
+ */
+
+//Admin
+Route::get('/admin/testing', function () {
+    return 'This is a test response!';
+});
+
+
+//Customer
+
+
+
+ /**
+ * 
+ * 
+ * 
+ */
