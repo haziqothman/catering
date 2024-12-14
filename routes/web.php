30666df30ef15d536c34d4f55bcc8bcc0fe31b72
azqddn <aziquddin99@gmail.com>
2024-12-14 14:38:09 +0800

    fixed route

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::prefix('customer')->middleware(['auth', 'user-access:customer'])->group(f
     Route::middleware(['auth'])->get('/customerProfile', [CustomerProfileController::class, 'show'])->name('customerProfile.show');
     Route::middleware(['auth'])->get('/customerProfile/edit', [CustomerProfileController::class, 'edit'])->name('customerProfile.edit');
     Route::middleware(['auth'])->post('/customerProfile/update', [CustomerProfileController::class, 'update'])->name('customerProfile.update');
+
+    /**
+     * Manage Catalogue
+     */
+    Route::get('/package/list', [CatalogueController::class, 'displayPackage'])->name('customer.display.package');
 });
 
 
@@ -40,7 +45,7 @@ All Admin Routes List
 --------------------------------------------*/
 Route::prefix('admin')->middleware(['auth', 'user-access:admin'])->group(function () {
     Route::get('/home', [HomeController::class, 'adminHome'])->name('admin.home');
-    
+
     Route::middleware(['auth'])->get('/profile', [AdminProfileController::class, 'show'])->name('adminProfile.show');
     Route::middleware(['auth'])->get('/profile/edit', [AdminProfileController::class, 'edit'])->name('adminProfile.edit');
     Route::middleware(['auth'])->post('/profile/update', [AdminProfileController::class, 'update'])->name('adminProfile.update');
@@ -49,27 +54,15 @@ Route::prefix('admin')->middleware(['auth', 'user-access:admin'])->group(functio
     Route::get('/users', [AdminProfileController::class, 'listUsers'])->name('adminProfile.users');
     Route::delete('/admin/users/{user}', [AdminProfileController::class, 'deleteUser'])->name('adminProfile.delete');
 
-});
-
-
-
-/**
- * 
- * Manage Catalogue
- * 
- */
-
- Route::get('/catalogue', [CatalogueController::class, 'index'])->name('catalogue.index');
-
-//Admin
-Route::group(['prefix' => 'admin/'], function () {
+    /**
+     * Manage Catalogue
+     */
     Route::get('/manage/package', [CatalogueController::class, 'displayManagePackage'])->name('admin.display.package');
-    Route::get('/package/edit', [CatalogueController::class, 'editPackage'])->name('admin.edit.package');
-});
-
-//Customer
-Route::group(['prefix' => 'customer/'], function(){
-    Route::get('/package/list', [CatalogueController::class, 'displayPackage'])->name('customer.display.package');
+    Route::get('/add/package', [CatalogueController::class, 'createPackage'])->name('admin.create.package');
+    Route::post('/store/package', [CatalogueController::class, 'storePackage'])->name('admin.store.package');
+    Route::get('/package/{id}/edit', [CatalogueController::class, 'editPackage'])->name('admin.edit.package');
+    Route::post('/update/{id}/package', [CatalogueController::class, 'updatePackage'])->name('admin.update.package');
+    Route::delete('/destroy/{id}/package', [CatalogueController::class, 'destroyPackage'])->name('admin.destroy.package');
 });
 
 
@@ -82,5 +75,3 @@ Route::group(['prefix' => 'customer/'], function(){
 //  Booking 
 
 Route::get('/booking', [BookingController::class, 'index'])->name('booking.index');
-
-
