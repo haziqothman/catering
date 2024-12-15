5ba6c931cc312e17d717982b0c57b8adb21fd33d
Aqilaaa22 <134986214+Aqilaaa22@users.noreply.github.com>
2024-12-16 01:37:02 +0800

    Updates

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,8 +10,10 @@
     {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.2.1/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+      
     </script>
     <style>
         body {
@@ -81,7 +83,11 @@
 
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Booking</a>
+                    @if (Auth::user()->type == 'admin')
+                        <a class="nav-link" href="{{ route('#') }}">Booking</a>
+                    @elseif (Auth::user()->type == 'customer')
+                        <a class="nav-link" href="{{ route('ManageBooking.Customer.dashboardBooking') }}">Booking</a>
+                    @endif
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Review</a>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ All Customer Routes List
 --------------------------------------------*/
 
 Route::middleware(['auth', 'user-access:customer'])->group(function () {
-<<<<<<<<< Temporary merge branch 1
     Route::group(['prefix' => 'customer/'], function () {
         Route::get('/home', [HomeController::class, 'index'])->name('home');
 
@@ -30,30 +29,31 @@ Route::middleware(['auth', 'user-access:customer'])->group(function () {
          */
         Route::get('/package/list', [CatalogueController::class, 'displayPackage'])->name('customer.display.package');
 
-
+        /**
+         * Manage Booking
+         */
+        Route::get('/customer/dashboard', [BookingController::class, 'show'])->name('ManageBooking.Customer.dashboardBooking');
+        Route::get('/booking/create', [BookingController::class, 'create'])->name('ManageBooking.Customer.createBooking');
+        Route::post('/customer/store-booking', [BookingController::class, 'store'])->name('customer.store.booking');
+        Route::get('/customer/booking/{id}/edit', [BookingController::class, 'edit'])->name('customer.edit.booking');
+        Route::put('/customer/booking/{id}', [BookingController::class, 'update'])->name('customer.update.booking');
+        Route::get('/booked-dates', [BookingController::class, 'getBookedDates'])->name('booked.dates');
+         
+        /**
+         * Manage Customer Profile
+         */
+        Route::get('/customerProfile', [CustomerProfileController::class, 'show'])->name('customerProfile.show');
+        Route::get('/customerProfile/edit', [CustomerProfileController::class, 'edit'])->name('customerProfile.edit');
+        Route::post('/customerProfile/update', [CustomerProfileController::class, 'update'])->name('customerProfile.update');
     });
-=========
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::middleware(['auth'])->get('/customerProfile', [CustomerProfileController::class, 'show'])->name('customerProfile.show');
-    Route::middleware(['auth'])->get('/customerProfile/edit', [CustomerProfileController::class, 'edit'])->name('customerProfile.edit');
-    Route::middleware(['auth'])->post('/customerProfile/update', [CustomerProfileController::class, 'update'])->name('customerProfile.update');
->>>>>>>>> Temporary merge branch 2
 });
 
-
-
-
-<<<<<<<<< Temporary merge branch 1
-=========
-
->>>>>>>>> Temporary merge branch 2
 /*------------------------------------------
 --------------------------------------------
 All Admin Routes List
 --------------------------------------------
 --------------------------------------------*/
 Route::middleware(['auth', 'user-access:admin'])->group(function () {
-<<<<<<<<< Temporary merge branch 1
     Route::group(['prefix' => 'admin/'], function () {
         Route::get('/home', [HomeController::class, 'adminHome'])->name('admin.home');
 
@@ -61,54 +61,26 @@ Route::middleware(['auth', 'user-access:admin'])->group(function () {
          * Manage Catalogue
          */
         Route::get('/manage/package', [CatalogueController::class, 'displayManagePackage'])->name('admin.display.package');
+        Route::get('/package/create', [CatalogueController::class, 'createPackage'])->name('admin.create.package');
+        Route::post('/package/store', [CatalogueController::class, 'storePackage'])->name('admin.store.package');
         Route::get('/package/{id}/edit', [CatalogueController::class, 'editPackage'])->name('admin.edit.package');
 
-        
+        /**
+         * Manage Admin Profile
+         */
+        Route::get('/adminProfile', [AdminProfileController::class, 'show'])->name('adminProfile.show');
+        Route::get('/adminProfile/edit', [AdminProfileController::class, 'edit'])->name('adminProfile.edit');
+        Route::post('/adminProfile/update', [AdminProfileController::class, 'update'])->name('adminProfile.update');
     });
+
 });
-
-
-=========
-   Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
-   Route::middleware(['auth'])->get('/adminProfile', [AdminProfileController::class, 'show'])->name('adminProfile.show');
-   Route::middleware(['auth'])->get('/adminProfile/edit', [AdminProfileController::class, 'edit'])->name('adminProfile.edit');
-   Route::middleware(['auth'])->post('/adminProfile/update', [AdminProfileController::class, 'update'])->name('adminProfile.update');
-});
-
-
 
 /**
- * 
- * Manage Catalogue
- * 
+ * Manage Catalogue (Common Routes)
  */
-
- Route::get('/catalogue', [CatalogueController::class, 'index'])->name('catalogue.index');
-
-//Admin
-Route::group(['prefix' => 'admin/'], function () {
-    Route::get('/manage/package', [CatalogueController::class, 'displayManagePackage'])->name('admin.display.package');
-    Route::get('/package/edit', [CatalogueController::class, 'editPackage'])->name('admin.edit.package');
-});
-
-//Customer
-Route::group(['prefix' => 'customer/'], function(){
-    Route::get('/package/list', [CatalogueController::class, 'displayPackage'])->name('customer.display.package');
-});
-
+Route::get('/catalogue', [CatalogueController::class, 'index'])->name('catalogue.index');
 
 /**
- * 
- * 
- * 
+ * Booking
  */
-
-//  Booking 
-
 Route::get('/booking', [BookingController::class, 'index'])->name('booking.index');
-
->>>>>>>>> Temporary merge branch 2
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
